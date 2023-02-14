a899affcf7c6fc835d8fb18df933b8fde3a7d09a
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-02-14 15:49:08 +0100

    refactor: add script for updating app.json-version based on package.json
    refs conjoon/conjoon#25

---
+++ bin/updateAppJson.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+/**
+ * conjoon
+ * Copyright (C) 2023 Thorsten Suckow-Homberg https://github.com/conjoon/conjoon
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+import fs from "fs-extra";
+import l8 from "@l8js/l8";
+
+
+function getValueFromPackageJson (dest, path) {
+    const pkg = (fs.readJsonSync(dest));
+    return l8.unchain(path, pkg);
+}
+
+function updateAppJson (dest, obj) {
+    let newPkg = fs.readJsonSync(dest);
+    newPkg = Object.assign(newPkg, obj);
+
+    fs.outputFileSync(dest, `${JSON.stringify(newPkg, null, 4)}`);
+}
+
+const version = getValueFromPackageJson("package.json", "version");
+
+// eslint-disable-next-line no-console
+console.log(`Updating app.json with ${version}...`);
+
+updateAppJson("app.json", {version});
+
+// eslint-disable-next-line no-undef
+process.exit(0);
